2d8753c06c53efec646ab465b1dff11b79c2c8ff
khou512 <76508068+khou512@users.noreply.github.com>
2022-05-17 21:08:12 -0400

    asdff

--- NelderMeade_regularizer_experiment.py
+++ NelderMeade_regularizer_experiment.py
@@ -80,10 +80,7 @@ else:
     raise TypeError("Invalid state type specified.")
 param_shape = (state.num_qubits, CIRCUIT_DEPTH+1, PARAM_PER_QUBIT_PER_DEPTH)
 
-# Visualize the distribution
-plt.bar(qubit_expansion(state.num_qubits), state.probabilities())
-plt.title(f"{NUM_QUBITS}-{STATE_TYPE} State Distribution (z-basis)")
-plt.show()
+
 
 
 
